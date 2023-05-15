9436b9248edaa51228bccbb08d3e1df1e624f0d4
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-16 01:25:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    arr[i] = 8;
+                    {
+                        Console.Write("Missing number is =" + arr[i]);
+
+
+
+
+                    }
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
+        
+    
+
